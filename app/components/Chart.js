842fd2375b6064c39cc68c53c6c288d41507b308
Vk4011 <gvikramram63@gmail.com>
2024-03-22 13:49:50 +0530

    graph

--- app/components/Chart.js
+++ app/components/Chart.js
@@ -1,53 +1,82 @@
-import React from "react";
-import { View, Text, Image, StyleSheet } from "react-native";
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, Image, Dimensions } from 'react-native';
+import { LineChart } from 'react-native-chart-kit';
 
-const Chart = ({
-  currency,
-  currentPriceChangePercentage,
-  logoUrl,
-  name,
-  symbol,
-  priceChangePercentage7d,
-  sparkline,
-  currentPrice,
-}) => {
+const { width: SIZE } = Dimensions.get('window');
 
-  const priceChangeColor = priceChangePercentage7d ? (priceChangePercentage7d > 0 ? 'green' : 'red') : 'black';
+const Chart = ({ currentPrice, logoUrl, name, symbol, priceChangePercentage7d, sparkline }) => {
+  const [chartReady, setChartReady] = useState(false);
 
+  const priceChangeColor = priceChangePercentage7d > 0 ? '#34C759' : '#FF3B30';
+
+  useEffect(() => {
+    setTimeout(() => {
+      setChartReady(true);
+    }, 0);
+  }, []);
+
+  if (sparkline.length === 0) {
+    return <Text>Loading...</Text>;
+  }
+
+  const data = {
+    labels: [],
+    datasets: [
+      {
+        data: sparkline,
+      },
+    ],
+  };
+
+  const chartConfig = {
+    backgroundGradientFrom: '#1E2923',
+    backgroundGradientFromOpacity: 0,
+    backgroundGradientTo: '#08130D',
+    backgroundGradientToOpacity: 0.5,
+    color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+    strokeWidth: 2,
+  };
 
   return (
     <View style={styles.chartWrapper}>
-    {/* Titles */}
-    <View style={styles.titlesWrapper}>
-      <View style={styles.upperTitles}>
-        <View style={styles.upperLeftTitle}>
-          <Image source={{ uri: logoUrl }} style={styles.image} />
-          <Text style={styles.subtitle}>
-            {name} ({symbol.toUpperCase()})
-          </Text>
+      {/* Titles */}
+      <View style={styles.titlesWrapper}>
+        <View style={styles.upperTitles}>
+          <View style={styles.upperLeftTitle}>
+            <Image source={{ uri: logoUrl }} style={styles.image} />
+            <Text style={styles.subtitle}>
+              {name} ({symbol.toUpperCase()})
+            </Text>
+          </View>
+          <Text style={styles.subtitle}>7d</Text>
         </View>
-        <Text style={styles.subtitle}>7d</Text>
+        <View style={styles.lowerTitles}>
+          <Text style={styles.boldTitle}>${currentPrice}</Text>
+          <Text style={[styles.title, { color: priceChangeColor }]}>{priceChangePercentage7d.toFixed(2)}%</Text>
+        </View>
       </View>
 
-      <View style={styles.lowerTitles}>
-        <Text style={styles.boldTitle}>${currentPrice.toLocaleString('en-US', { currency: 'USD' })}</Text>
-        {/* Check if priceChangePercentage7d is defined before using it */}
-        <Text style={[styles.title, { color: priceChangeColor }]}>
-          {priceChangePercentage7d ? priceChangePercentage7d.toFixed(2) + "%" : "N/A"}
-        </Text>
-      </View>
-    </View>
-     
+      {chartReady ? (
+        <View style={styles.chartLineWrapper}>
+          <LineChart
+            data={data}
+            width={SIZE}
+            height={220}
+            chartConfig={chartConfig}
+            bezier
+          />
+        </View>
+      ) : null}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   chartWrapper: {
-    marginVertical: 16
+    marginVertical: 16,
   },
   titlesWrapper: {
-    marginHorizontal: 16
+    marginHorizontal: 16,
   },
   upperTitles: {
     flexDirection: 'row',
@@ -59,8 +88,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   image: {
-    width: 44,
-    height: 44,
+    width: 24,
+    height: 24,
     marginRight: 4,
   },
   subtitle: {
@@ -83,6 +112,5 @@ const styles = StyleSheet.create({
     marginTop: 40,
   },
 });
-
 
 export default Chart;
